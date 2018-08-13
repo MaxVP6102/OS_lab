aed52af4d64a462d1bb6a77d60f3fc76b681d14d
MaxVP6102 <maxvp6102@gmail.com>
2018-08-13 15:41:11 +0530

    Lab 02

---
+++ Lab02/Fork.cpp
@@ -0,0 +1,44 @@
+/* 
+WAP to create a process using Fork and display the pid for the process
+
+*/
+#include  <stdio.h>
+#include  <sys/types.h>
+#include <unistd.h>
+
+#define   MAX_COUNT  200
+
+void  ChildProcess(void);                /* child process prototype  */
+void  ParentProcess(void);               /* parent process prototype */
+
+int  main()
+{
+     pid_t  pid;
+
+     pid = fork();
+     if (pid == 0) 
+          ChildProcess();
+     else 
+          ParentProcess();
+
+
+    return 0;
+}
+
+void  ChildProcess(void)
+{
+     int   i;
+
+     for (i = 1; i <= MAX_COUNT; i++)
+          printf("   This line is from child, value = %d\n", i);
+     printf("   *** Child process is done ***\n");
+}
+
+void  ParentProcess(void)
+{
+     int   i;
+
+     for (i = 1; i <= MAX_COUNT; i++)
+          printf("This line is from parent, value = %d\n", i);
+     printf("*** Parent is done ***\n");
+}
